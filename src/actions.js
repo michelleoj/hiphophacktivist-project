4982bb3cdcc2c5c58ca9877e11586c38cea4872c
Deary Hudson <deary@knotch.com>
2017-06-24 18:13:53 -0400

    added method to filter shelters - by text search query and nyc borough - by organization name

--- src/actions.js
+++ src/actions.js
@@ -1,6 +1,8 @@
 export const FETCH_SHELTERS = 'FETCH_SHELTERS';
 export const RECEIVE_SHELTERS = 'RECEIVE_SHELTERS';
 const appToken = 'wcZxN03tedKhA6e528L5K9LsQ'
+const endpoint = 'https://data.cityofnewyork.us/resource/386y-9exk.json'
+
 
 // regular actions
 export const receiveShelterObjects = (shelters) => {
@@ -26,5 +28,40 @@ export const fetchShelters = (query) => {
     .catch((err) => {
       console.log('something went wrong');
     });
+  }
+}
+
+export const fetchSheltersForSearch = (query) => {
+  return function(dispatch) {
+    query = query || '';
+
+    fetch(`${endpoint}?$$app_token=${appToken}&$where=(description LIKE %${query}% OR organizationname LIKE %${query}%)`)
+    .then(response => {
+      response.json().then(json => {
+        dispatch(receiveShelterObjects(json));
+      });
+    })
+    .catch(err => {
+      console.log(`Something went wrong: ${err.message}`);
+    });
+  }
+}
+
+export const fetchShelterInfo = (query) => {
+  return function(dispatch) {
+    query = query || '';
+
+    fetch(`${endpoint}?$app_token${appToken}&$where=organizationname = ${query}`)
+      .then(response => {
+        response.json().then(json => {
+          // all responses are enveloped in arrays, so we need to pop the stack
+          // to get the organization object.
+          const org = (response.length > 0) ? response.pop() : null;
+          dispatch(receiveShelterObjects(org));
+        });
+      })
+      .catch(err => {
+        console.log(`Something went wrong: ${err.message}`);
+      });
   }
 }
